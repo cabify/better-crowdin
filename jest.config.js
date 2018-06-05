43108401934b59a4d6fb23079e43a531c9ffb067
cicloon <aleon.prof@gmail.com>
2018-06-05 13:13:36 +0200

    Added module-directories

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   rootDir: path.resolve(__dirname),
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['js', 'json'],
+  moduleDirectories: ['lib'],
   globals: {},
   testMatch: ['**/test/**/*?(*.)(Test).(js|ts|tsx)'],
   transform: {
--- test/unit/apiTest.js
+++ test/unit/apiTest.js
@@ -1,4 +1,4 @@
-const api = require('../../lib/api.js');
+const api = require('../../lib/api');
 
 let mockRequestSpy;
 
